6d71598cc782f45d76bd1c37b0577062be4ac57e
jordan4o <jdukadinov@gmail.com>
2016-05-24 16:16:31 +0200

    fix initial image size (load images after layout)

--- app/src/main/java/de/dukadinov/lizer/DemoContentProvider.java
+++ app/src/main/java/de/dukadinov/lizer/DemoContentProvider.java
@@ -17,14 +17,7 @@ public class DemoContentProvider implements LizerFragment.Input {
     }
 
     public Drawable getNextImage(int width, int height) {
-        if (width == 0) {
-            width = 500;
-        }
-        if (height == 0) {
-            height = 500;
-        }
-
-        if (++index == imageIds.length) {
+        if (++index == imageIds.length) { // rotate images
             index = 0;
         }
         Bitmap bitmap = ImageLoader.getImageFromApp(imageIds[index], width, height, resources);
